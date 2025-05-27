da67cd73e377dc5e7259ccc2928696e788fa998b
seungin <tmddls91@inha.edu>
2025-05-27 19:54:49 +0900

    [Feat]: 최단 경로 도착점에서 가장 가까운 출입구까지 연결 기능 구현

--- src/main/java/com/inha/capstone/capstone/repository/GatePointRepository.java
+++ src/main/java/com/inha/capstone/capstone/repository/GatePointRepository.java
@@ -4,9 +4,11 @@ import com.inha.capstone.capstone.entity.GatePoint;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface GatePointRepository extends JpaRepository<GatePoint, Long> {
     Optional<GatePoint> findByName(String name);
+    List<GatePoint> findByNameContaining(String query);
 }
