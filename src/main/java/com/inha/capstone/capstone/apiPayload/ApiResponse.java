cf48c7b16694763229a2a8e6c2b723aa75d9e4f8
seungin <tmddls91@inha.edu>
2025-06-05 09:47:54 +0900

    [Fix]: ApiResponse 클래스 중복 선언 고치기

--- src/main/java/com/inha/capstone/capstone/apiPayload/ApiResponse.java
+++ src/main/java/com/inha/capstone/capstone/apiPayload/ApiResponse.java
@@ -7,34 +7,30 @@ import com.inha.capstone.capstone.apiPayload.code.BaseCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-public class ApiResponse {
-    @Getter
-    @AllArgsConstructor
-    @JsonPropertyOrder({"isSuccess", "code", "message", "result"})
-    public class ApiResponse<T> {
+@Getter
+@AllArgsConstructor
+@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
+public class ApiResponse<T> {
+    @JsonProperty("isSuccess")
+    private final Boolean isSuccess;
+    private final String code;
+    private final String message;
 
-        @JsonProperty("isSuccess")
-        private final Boolean isSuccess;
-        private final String code;
-        private final String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T result;
 
-        @JsonInclude(JsonInclude.Include.NON_NULL)
-        private T result;
-
-        /*
-        public static <T> ApiResponse<T> onSuccess(T result) {
-            return new ApiResponse<>(true, SuccessStatus._OK.getCode(), SuccessStatus._OK.getMessage(), result);
-        }
-
-        public static <T> ApiResponse<T> of(BaseCode code, T result) {
-            return new ApiResponse<>(true, code.getReasonHttpStatus().getCode(), code.getReasonHttpStatus().getMessage(), result);
-        }
-
-        // 실패
-        public static <T> ApiResponse<T> onFailure(String code, String message, T data) {
-            return new ApiResponse<>(false, code, message, data);
-        }
-        */
-
+    /*
+    public static <T> ApiResponse<T> onSuccess(T result) {
+        return new ApiResponse<>(true, SuccessStatus._OK.getCode(), SuccessStatus._OK.getMessage(), result);
     }
+
+    public static <T> ApiResponse<T> of(BaseCode code, T result) {
+        return new ApiResponse<>(true, code.getReasonHttpStatus().getCode(), code.getReasonHttpStatus().getMessage(), result);
+    }
+
+     // 실패
+    public static <T> ApiResponse<T> onFailure(String code, String message, T data) {
+        return new ApiResponse<>(false, code, message, data);
+    }
+    */
 }
