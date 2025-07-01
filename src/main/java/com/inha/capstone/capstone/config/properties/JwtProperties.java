80d0b83b59f95f84c470fdf7c9d16d4e5d6f0f0e
seungin <tmddls91@inha.edu>
2025-07-02 04:52:11 +0900

    [Chore] JWT 의존성 및 인증 관련 상수 클래스 추가

--- src/main/java/com/inha/capstone/capstone/config/properties/JwtProperties.java
+++ src/main/java/com/inha/capstone/capstone/config/properties/JwtProperties.java
@@ -1,4 +1,4 @@
-package com.inha.capstone.capstone.config;
+package com.inha.capstone.capstone.config.properties;
 
 import lombok.Getter;
 import lombok.Setter;
---
+++ src/main/java/com/inha/capstone/capstone/config/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,4 @@
+package com.inha.capstone.capstone.config.security.jwt;
+
+public class JwtAuthenticationFilter {
+}
